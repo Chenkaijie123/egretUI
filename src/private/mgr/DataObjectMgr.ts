008ab29ae1d186027a2dadab289898a97f649278
Chenkaijie123 <1721225745@qq.com>
2019-05-15 22:24:29 +0800

    Signed-off-by: Chenkaijie123 <1721225745@qq.com>

--- src/private/mgr/DataObjectMgr.ts
+++ src/private/mgr/DataObjectMgr.ts
@@ -1,4 +1,24 @@
-class DataObjectMgr {
-	public constructor() {
+
+
+namespace mgr {
+	export class DataObjectMgr implements IMgr{
+		private pool:Array<Object> = [];
+		public constructor() {
+		}
+		public init():void{}
+
+		public pop<T extends Object>():T{
+			let r = this.pool.pop() || Object.create(null);
+			return r;
+		}
+
+		public push(...item:any[]):void{
+			for(let i of item){
+				for(let k in i){
+					delete i[k]
+				}
+				this.pool.push(i);
+			}
+		}
 	}
 }
